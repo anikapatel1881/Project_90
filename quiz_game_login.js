3e979477b25ddee63944693cb005ba043d78a9ac
anikapatel1881 <102267255+anikapatel1881@users.noreply.github.com>
2022-07-22 16:56:58 -0500

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,6 +1,6 @@
 function addUser() {
-    player_1_name = document.getElementById("player1_name_imput").value;
-    player_2_name = document.getElementById("player2_name_imput").value;
+    player_1_name = document.getElementById("player1_name_input").value;
+    player_2_name = document.getElementById("player2_name_input").value;
 
     localStorage.setItem("player1_name", player_1_name);
     localStorage.setItem("player2_name", player_2_name);
